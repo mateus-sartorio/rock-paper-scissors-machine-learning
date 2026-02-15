c5d10690c383bf280e3f41759c9a1e0aa61722a9
mateus-sartorio <mateussartorio39@gmail.com>
2026-02-14 22:34:39 -0300

    feat: mostly winning stragegy with Markov Chains

--- RPS.py
+++ RPS.py
@@ -1,10 +1,40 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+moves_sequences_map = {}
+N = 5
+
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    global moves_sequences_map
+    global N
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if prev_play == "":
+        moves_sequences_map = {}
+        opponent_history.clear()
 
-    return guess
+    if prev_play != "":
+        opponent_history.append(prev_play)
+
+    if len(opponent_history) < N:
+        return "R"
+    
+    last_pattern = "".join(opponent_history[-N:])
+
+    if last_pattern in moves_sequences_map:
+        moves_sequences_map[last_pattern] += 1
+    else:
+        moves_sequences_map[last_pattern] = 1
+    
+    current_pattern = "".join(opponent_history[-(N-1):])
+
+    potential_plays = [
+        current_pattern + "R",
+        current_pattern + "P",
+        current_pattern + "S"
+    ]
+
+    predicted_enemy_next_play = max(potential_plays, key=lambda k: moves_sequences_map.get(k, 0))[-1]
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    prediction = ideal_response[predicted_enemy_next_play]
+
+    return prediction
